e0fd2d9accd3944e9d128258716c9506c45416f4
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2013-04-02 15:13:17 +0200

    Added manually corrected azure bing api libraries and its dependencies. Added test folder.

---
+++ test/info/ephyra/search/searchers/BingNewKMTest.java
@@ -0,0 +1,20 @@
+package info.ephyra.search.searchers;
+
+import info.ephyra.querygeneration.Query;
+import info.ephyra.search.Result;
+
+import org.junit.Test;
+
+public class BingNewKMTest
+{
+
+	@Test
+	public void testDoSearch()
+	{
+		BingNewKM km = new BingNewKM();
+		km.query = new Query("Das offizielle Internet-Portal für Leipziger Bürger");
+		Result result = km.doSearch()[0];
+		System.out.println(result);
+	}
+
+}
